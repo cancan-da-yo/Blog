3b411e75a35273aad2a8a339e4e1d4d46e8731b6
cancan-da-yo <cankang2416@gmail.com>
2025-07-18 11:24:02 +0800

    add_netlify

---
+++ netlify/functions/rebuild.js
@@ -0,0 +1,10 @@
+export async function handler(event, context) {
+  const res = await fetch("https://api.netlify.com/build_hooks/6879b761109255663ad649b8", {
+    method: "POST",
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Triggered site rebuild", response: await res.text() }),
+  };
+}
